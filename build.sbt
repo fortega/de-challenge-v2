8f70a3c690fc91ddbf9a480ea3be5036bb499e72
Felipe Ortega <felipeortegabustamante@gmail.com>
2022-02-23 20:04:23 -0300

    feat: scala structure

---
+++ build.sbt
@@ -0,0 +1,5 @@
+scalaVersion := "2.12.13"
+
+libraryDependencies ++= Seq(
+    "org.apache.spark" %% "spark-sql" % "3.2.1",
+)
---
+++ src/main/scala/fortega/App.scala
@@ -0,0 +1,7 @@
+package fortega
+
+object App {
+    def main(cmdArgs: Array[String]): Unit = {
+        println("hello world")
+    }
+}
