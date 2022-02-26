fe5fba2dd9fa23697bbb35d2e03e2d46c18fde68
Felipe Ortega <felipeortegabustamante@gmail.com>
2022-02-26 18:56:22 -0300

    test: add DataSample to match dataset structure. add TransformationListTest

--- src/test/scala/fortega/Utils.scala
+++ src/test/scala/fortega/Utils.scala
@@ -9,7 +9,10 @@ object Utils {
 
     lazy val data = {
         import spark.implicits._
-        Seq("test1", "test2").toDF
+        Seq(
+            DataSample("E0", "18/01/10","Team1","Team2", 1, 1, "D", 10, 20),
+            DataSample("E0", "18/01/10","Team2","Team1", 3, 1, "H", 60, 10)
+        ).toDF
     }
 
     def isLocalMaster(spark: SparkSession) =  spark.sparkContext.master.startsWith("local")
@@ -22,4 +25,6 @@ object Utils {
                 .foreach(deleteDir)
         file.delete
     }
+
+    case class DataSample(div: String, date: String, homeTeam: String, awayTeam: String, fthg: Int, ftag: Int, ftr: String, hst: Int, ast: Int)
 }
